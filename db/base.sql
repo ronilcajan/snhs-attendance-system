8b2d7919c4cc1d03c62280c67aadd282d38ddc51
Ronil Cajan <cajanr02@gmail.com>
2024-06-26 17:08:37 +0800

    Update base.sql

--- db/base.sql
+++ db/base.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 02, 2021 at 12:56 PM
--- Server version: 10.4.20-MariaDB
--- PHP Version: 7.4.21
+-- Generation Time: Jan 30, 2022 at 01:41 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.0.13
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,8 +18,47 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `base`
+-- Database: `snhs`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attendance`
+--
+
+CREATE TABLE `attendance` (
+  `id` int(11) NOT NULL,
+  `date` date DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `morning_in` time DEFAULT NULL,
+  `morning_out` time DEFAULT NULL,
+  `afternoon_in` time DEFAULT NULL,
+  `afternoon_out` time DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `attendance`
+--
+
+INSERT INTO `attendance` (`id`, `date`, `email`, `morning_in`, `morning_out`, `afternoon_in`, `afternoon_out`) VALUES
+(3, '2021-10-01', 'diosdado.villacampa@deped.gov.ph', '07:30:00', '12:00:00', '12:30:00', '16:30:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `biometrics`
+--
+
+CREATE TABLE `biometrics` (
+  `id` int(11) NOT NULL,
+  `date` date DEFAULT NULL,
+  `am_in` time DEFAULT NULL,
+  `am_out` time DEFAULT NULL,
+  `pm_in` time DEFAULT NULL,
+  `pm_out` time DEFAULT NULL,
+  `bio_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -57,6 +96,32 @@ CREATE TABLE `login_attempts` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `personnels`
+--
+
+CREATE TABLE `personnels` (
+  `id` int(11) NOT NULL,
+  `firstname` varchar(50) DEFAULT NULL,
+  `lastname` varchar(50) DEFAULT NULL,
+  `middlename` varchar(50) DEFAULT NULL,
+  `position` varchar(30) DEFAULT NULL,
+  `role` varchar(50) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `fb` text DEFAULT NULL,
+  `status` int(11) DEFAULT 1,
+  `bio_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `personnels`
+--
+
+INSERT INTO `personnels` (`id`, `firstname`, `lastname`, `middlename`, `position`, `role`, `email`, `fb`, `status`, `bio_id`) VALUES
+(170, 'NIUBIE', 'ALTAYA', 'DELUMBAR', 'T III', NULL, 'niubie.altaya@deped.gov.ph', 'https://www.facebook.com/niubie.altaya.5', 1, 16);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `systems`
 --
 
@@ -72,7 +137,7 @@ CREATE TABLE `systems` (
 --
 
 INSERT INTO `systems` (`id`, `system_logo`, `system_name`, `system_acronym`) VALUES
-(1, '0d2e21a10fb3d0452cc6fabfc38e92c8.png', 'Pharma System', 'Pharma');
+(1, 'b5ef89d7e97a6162a705fcd1432a06a6.jpg', 'Time Record System', 'WFH');
 
 -- --------------------------------------------------------
 
@@ -107,7 +172,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `ip_address`, `username`, `password`, `email`, `activation_selector`, `activation_code`, `forgotten_password_selector`, `forgotten_password_code`, `forgotten_password_time`, `remember_selector`, `remember_code`, `created_on`, `last_login`, `active`, `first_name`, `last_name`, `address`, `avatar`) VALUES
-(2, '::1', 'admin', '$2y$10$yNOenO2kp5FrRKgM/V78o.JFpfK2kJli9UMmAwI1fMQW8J/lzcE22', 'cajanr02@gmail.com', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1628636671, 1633148593, 1, 'Roni', 'Cajan', 'Looc Proper', '81cb8d65a41192af7d318fcc3c8cca8c.png');
+(2, '::1', 'admin', '$2y$10$yNOenO2kp5FrRKgM/V78o.JFpfK2kJli9UMmAwI1fMQW8J/lzcE22', 'cajanr02@gmail.com', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1628636671, 1643541754, 1, 'Roni', 'Cajan', 'Looc Proper', '81cb8d65a41192af7d318fcc3c8cca8c.png'),
+(3, '2001:4455:5b4:4300:418:56c8:7a1:3950', 'hrofficer', '$2y$10$L0e5/wNaBcb3nc3VmQLMGeCZiWRygy5mHfNCq9U0DrVw6qc6axMje', 'arnoldjr.amboang@deped.gov.ph', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1641969906, 1641969982, 1, 'Arnold Jr', 'Amboang', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -126,11 +192,24 @@ CREATE TABLE `users_groups` (
 --
 
 INSERT INTO `users_groups` (`id`, `user_id`, `group_id`) VALUES
-(8, 2, 1);
+(9, 2, 1),
+(10, 3, 1);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `attendance`
+--
+ALTER TABLE `attendance`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `biometrics`
+--
+ALTER TABLE `biometrics`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `groups`
@@ -142,6 +221,12 @@ ALTER TABLE `groups`
 -- Indexes for table `login_attempts`
 --
 ALTER TABLE `login_attempts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `personnels`
+--
+ALTER TABLE `personnels`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -174,6 +259,18 @@ ALTER TABLE `users_groups`
 --
 
 --
+-- AUTO_INCREMENT for table `attendance`
+--
+ALTER TABLE `attendance`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1011;
+
+--
+-- AUTO_INCREMENT for table `biometrics`
+--
+ALTER TABLE `biometrics`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
+
+--
 -- AUTO_INCREMENT for table `groups`
 --
 ALTER TABLE `groups`
@@ -186,6 +283,12 @@ ALTER TABLE `login_attempts`
   MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
+-- AUTO_INCREMENT for table `personnels`
+--
+ALTER TABLE `personnels`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=227;
+
+--
 -- AUTO_INCREMENT for table `systems`
 --
 ALTER TABLE `systems`
@@ -195,24 +298,13 @@ ALTER TABLE `systems`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `users_groups`
 --
 ALTER TABLE `users_groups`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `users_groups`
---
-ALTER TABLE `users_groups`
-  ADD CONSTRAINT `fk_users_groups_groups1` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_users_groups_users1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
